e88a816410b780ff3662de2d064e45a4c2b98fb3
mbaeta8 <mbaeta8@gmail.com>
2024-11-22 15:25:06 +0100

    Animaciones de jump y walk arregladas

--- mario.gd
+++ mario.gd
@@ -11,38 +11,31 @@ var horizontal:int
 var en_suelo:bool
 var tocando_escalera:bool
 
-func _ready() -> void:
+func _ready():
 	estado_actual = Estados.IDLE
 	tocando_escalera = false
 
-func _process(_delta:float) -> void:
-	if tocando_escalera:
-		if en_suelo:
-			if Input.is_action_just_pressed("bajar"):
-				estado_actual = Estados.EN_ESCALERA
+func _process(_delta):
+	if estado_actual != Estados.EN_ESCALERA:
+		if not en_suelo:
+			estado_actual = Estados.SALTANDO
+		elif horizontal != 0:
+			estado_actual = Estados.CORRIENDO
 		else:
-			estado_actual = Estados.EN_ESCALERA
-	
-		if estado_actual != Estados.EN_ESCALERA:
-			if linear_velocity == Vector2.ZERO:
-				estado_actual = Estados.IDLE
-			elif floor(linear_velocity.y) < -5:
-				estado_actual = Estados.SALTANDO
-			elif horizontal != 0:
-				estado_actual = Estados.CORRIENDO
+			estado_actual = Estados.IDLE
 	
 	match estado_actual:
 		Estados.IDLE:
-			$Animaciones.play("idle")
+			$AnimatedSprite.play("idle")
 		Estados.SALTANDO:
-			$Animaciones.play("jump")
+			$AnimatedSprite.play("jump")
 		Estados.CORRIENDO:
-			$Animaciones.play("walk")
+			$AnimatedSprite.play("walk")
 		Estados.EN_ESCALERA:
 			if linear_velocity.y == 0:
-				$Animaciones.stop()
+				$AnimatedSprite.stop()
 			else:
-				$Animaciones.play("stairs")
+				$AnimatedSprite.play("stairs")
 	
 	if en_suelo and estado_actual != Estados.EN_ESCALERA:
 		if Input.is_action_pressed("mover_izquierda"):
@@ -53,9 +46,9 @@ func _process(_delta:float) -> void:
 			horizontal = 0
 	
 	if horizontal < 0:
-		$Animaciones.flip_h = true
+		$AnimatedSprite.flip_h = true
 	elif horizontal > 0:
-		$Animaciones.flip_h = false
+		$AnimatedSprite.flip_h = false
 	
 	if estado_actual == Estados.EN_ESCALERA:
 		horizontal = 0
@@ -79,7 +72,7 @@ func _integrate_forces(state) -> void:
 		gravity_scale = 0
 		if Input.is_action_pressed("saltar"):
 			state.linear_velocity.y = -VELOCIDAD_ESCALERA
-		elif Input.is_action_pressed("bajar"):
+		elif Input.is_action_pressed("bajar_escalera"):
 			state.linear_velocity.y = VELOCIDAD_ESCALERA
 		else:
 			state.linear_velocity.y = 0
@@ -93,9 +86,13 @@ func _integrate_forces(state) -> void:
 func _on_Cuerpo_area_entered(area):
 	if area.name == "Escaleras":
 		tocando_escalera = true
+		estado_actual = Estados.EN_ESCALERA
 
 
 func _on_Cuerpo_area_exited(area):
 	if area.name == "Escaleras":
 		tocando_escalera = false
-		estado_actual = Estados.IDLE
+		if en_suelo:
+			estado_actual = Estados.IDLE
+		else: 
+			estado_actual = Estados.SALTANDO
